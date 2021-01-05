3bcd5c229f6535ee14f37dca5d158309ff8ec86b
Daniel Bunte <daniel.bunte@accenture.com>
2021-01-05 14:16:58 +0100

    removed TaxiNode.plus

--- src/main/kotlin/io/twodigits/demo/groundoperations/taxi/TaxiNode.kt
+++ src/main/kotlin/io/twodigits/demo/groundoperations/taxi/TaxiNode.kt
@@ -1,8 +1,3 @@
 package io.twodigits.demo.groundoperations.taxi
 
 data class TaxiNode(val taxiway: String, val turn: String? = null, var next: TaxiNode? = null)
-
-operator fun TaxiNode.plus(node: TaxiNode): TaxiNode {
-    this.next = node
-    return node
-}
